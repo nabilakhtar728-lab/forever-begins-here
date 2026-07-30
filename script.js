ab739a0d8eab2a795910f01d909ea7eb892e27c8
nabilakhtar728-lab <nabilakhtar728@gmail.com>
2026-07-31 00:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const startBtn = document.getElementById("startBtn");
 
+startBtn.addEventListener("click", () => {
+    startBtn.innerText = "Loading Our Love Story... ❤️";
+    startBtn.disabled = true;
+
+    document.body.style.transition = "1.5s";
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+        alert(
+`❤️ Welcome Tahura ❤️
+
+Our journey began on
+10 August 2022
+
+This is only the beginning...
+
+A beautiful surprise is waiting for you. 💍`
+        );
+
+        document.body.style.opacity = "1";
+        startBtn.innerText = "Begin Our Story ❤️";
+        startBtn.disabled = false;
+    }, 1500);
+});
